34ec729c5614af07d669d9f378aa46467f1e594e
Daniel Micay <danielmicay@gmail.com>
2016-11-24 11:41:00 -0500

    disable metrics by default

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunActivityBase.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunActivityBase.java
@@ -63,7 +63,7 @@ public abstract class FirstRunActivityBase extends AsyncInitializationActivity
     static final String SHOW_SYNC_CONSENT_PAGE = "ShowSyncConsent";
     static final String SHOW_HISTORY_SYNC_PAGE = "ShowHistorySync";
 
-    public static final boolean DEFAULT_METRICS_AND_CRASH_REPORTING = true;
+    public static final boolean DEFAULT_METRICS_AND_CRASH_REPORTING = false;
 
     private static PolicyLoadListenerFactory sPolicyLoadListenerFactoryForTesting;
 
