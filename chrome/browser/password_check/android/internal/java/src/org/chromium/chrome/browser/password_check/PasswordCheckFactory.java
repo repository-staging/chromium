f1b8b1ba203de95c6681f832f852c7de587387f5
Daniel Micay <danielmicay@gmail.com>
2021-04-15 11:33:17 -0400

    disable unused password check feature

--- chrome/browser/password_check/android/internal/java/src/org/chromium/chrome/browser/password_check/PasswordCheckFactory.java
+++ chrome/browser/password_check/android/internal/java/src/org/chromium/chrome/browser/password_check/PasswordCheckFactory.java
@@ -24,10 +24,7 @@ public class PasswordCheckFactory {
      * @return A {@link PasswordCheckImpl} or null if the feature is disabled.
      */
     public static @Nullable PasswordCheck getOrCreate() {
-        if (sPasswordCheck == null) {
-            sPasswordCheck = new PasswordCheckImpl();
-        }
-        return sPasswordCheck;
+       return null;
     }
 
     /**
